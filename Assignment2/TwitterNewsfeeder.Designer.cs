9f512288413e9474ed393b9583f65c8ec4454039
Lucy Kang <HaeYeon.Kang@MyGeorgian.ca>
2016-05-18 10:12:35 -0400

    Add project files.

---
+++ Assignment2/TwitterNewsfeeder.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Assignment2
+{
+    partial class twitterNewsfeeder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(twitterNewsfeeder));
+            this.hashTagBtn = new System.Windows.Forms.Button();
+            this.twitterMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.twitterHandleLabel = new System.Windows.Forms.Label();
+            this.handleTextBox = new System.Windows.Forms.TextBox();
+            this.numOfTweetsTextBox = new System.Windows.Forms.TextBox();
+            this.numOfTweetsLabel = new System.Windows.Forms.Label();
+            this.userBtn = new System.Windows.Forms.Button();
+            this.resultRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.twitterTooltip = new System.Windows.Forms.ToolTip(this.components);
+            this.twitterMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // hashTagBtn
+            // 
+            this.hashTagBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.hashTagBtn.Location = new System.Drawing.Point(430, 68);
+            this.hashTagBtn.Name = "hashTagBtn";
+            this.hashTagBtn.Size = new System.Drawing.Size(150, 27);
+            this.hashTagBtn.TabIndex = 3;
+            this.hashTagBtn.Text = "Search by Hash&Tag";
+            this.twitterTooltip.SetToolTip(this.hashTagBtn, "Search tweets by hashtag");
+            this.hashTagBtn.UseVisualStyleBackColor = true;
+            this.hashTagBtn.Click += new System.EventHandler(this.hashTagBtn_Click);
+            // 
+            // twitterMenuStrip
+            // 
+            this.twitterMenuStrip.BackColor = System.Drawing.Color.LightPink;
+            this.twitterMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.twitterMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.twitterMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.twitterMenuStrip.Name = "twitterMenuStrip";
+            this.twitterMenuStrip.Size = new System.Drawing.Size(592, 28);
+            this.twitterMenuStrip.TabIndex = 1;
+            this.twitterMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(164, 26);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(126, 26);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // twitterHandleLabel
+            // 
+            this.twitterHandleLabel.AutoSize = true;
+            this.twitterHandleLabel.Location = new System.Drawing.Point(13, 44);
+            this.twitterHandleLabel.Name = "twitterHandleLabel";
+            this.twitterHandleLabel.Size = new System.Drawing.Size(179, 15);
+            this.twitterHandleLabel.TabIndex = 2;
+            this.twitterHandleLabel.Text = "Twitter Handle or HashTag";
+            // 
+            // handleTextBox
+            // 
+            this.handleTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.handleTextBox.Location = new System.Drawing.Point(16, 70);
+            this.handleTextBox.Name = "handleTextBox";
+            this.handleTextBox.Size = new System.Drawing.Size(300, 25);
+            this.handleTextBox.TabIndex = 0;
+            this.twitterTooltip.SetToolTip(this.handleTextBox, "Please enter a username or hashtag to search.");
+            // 
+            // numOfTweetsTextBox
+            // 
+            this.numOfTweetsTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numOfTweetsTextBox.Location = new System.Drawing.Point(341, 70);
+            this.numOfTweetsTextBox.Name = "numOfTweetsTextBox";
+            this.numOfTweetsTextBox.Size = new System.Drawing.Size(64, 25);
+            this.numOfTweetsTextBox.TabIndex = 1;
+            this.twitterTooltip.SetToolTip(this.numOfTweetsTextBox, "Please enter number of tweets to see");
+            // 
+            // numOfTweetsLabel
+            // 
+            this.numOfTweetsLabel.AutoSize = true;
+            this.numOfTweetsLabel.Location = new System.Drawing.Point(339, 44);
+            this.numOfTweetsLabel.Name = "numOfTweetsLabel";
+            this.numOfTweetsLabel.Size = new System.Drawing.Size(66, 15);
+            this.numOfTweetsLabel.TabIndex = 5;
+            this.numOfTweetsLabel.Text = "# Tweets";
+            // 
+            // userBtn
+            // 
+            this.userBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.userBtn.Location = new System.Drawing.Point(430, 35);
+            this.userBtn.Name = "userBtn";
+            this.userBtn.Size = new System.Drawing.Size(150, 27);
+            this.userBtn.TabIndex = 2;
+            this.userBtn.Text = "Search by &User";
+            this.twitterTooltip.SetToolTip(this.userBtn, "Search tweets by username");
+            this.userBtn.UseVisualStyleBackColor = true;
+            this.userBtn.Click += new System.EventHandler(this.userBtn_Click);
+            // 
+            // resultRichTextBox
+            // 
+            this.resultRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultRichTextBox.BackColor = System.Drawing.Color.White;
+            this.resultRichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.resultRichTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.resultRichTextBox.Location = new System.Drawing.Point(16, 112);
+            this.resultRichTextBox.Name = "resultRichTextBox";
+            this.resultRichTextBox.ReadOnly = true;
+            this.resultRichTextBox.Size = new System.Drawing.Size(564, 242);
+            this.resultRichTextBox.TabIndex = 7;
+            this.resultRichTextBox.TabStop = false;
+            this.resultRichTextBox.Text = "";
+            this.twitterTooltip.SetToolTip(this.resultRichTextBox, "Results will be shown when you click a button");
+            // 
+            // twitterNewsfeeder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(592, 375);
+            this.Controls.Add(this.resultRichTextBox);
+            this.Controls.Add(this.userBtn);
+            this.Controls.Add(this.numOfTweetsLabel);
+            this.Controls.Add(this.numOfTweetsTextBox);
+            this.Controls.Add(this.handleTextBox);
+            this.Controls.Add(this.twitterHandleLabel);
+            this.Controls.Add(this.hashTagBtn);
+            this.Controls.Add(this.twitterMenuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.twitterMenuStrip;
+            this.Name = "twitterNewsfeeder";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Twitter Newsfeeder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.twitterMenuStrip.ResumeLayout(false);
+            this.twitterMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button hashTagBtn;
+        private System.Windows.Forms.MenuStrip twitterMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label twitterHandleLabel;
+        private System.Windows.Forms.TextBox handleTextBox;
+        private System.Windows.Forms.TextBox numOfTweetsTextBox;
+        private System.Windows.Forms.Label numOfTweetsLabel;
+        private System.Windows.Forms.Button userBtn;
+        private System.Windows.Forms.RichTextBox resultRichTextBox;
+        private System.Windows.Forms.ToolTip twitterTooltip;
+    }
+}
+
---
+++ Assignment2/TwitterNewsfeeder.cs
@@ -0,0 +1,169 @@
+﻿/*
+Author: Hae Yeon (Lucy) Kang
+Date: 2016-03-06
+Application: Assignment2
+Description: This application will display tweets from other user's timeline or by hashtag.
+*/
+
+using System;
+using System.Linq;
+using System.Windows.Forms;
+using TweetSharp;
+
+namespace Assignment2
+{
+    public partial class twitterNewsfeeder : Form
+    {
+        TwitterService sTwitterService; //interact with Twitter
+
+        //keys to access to the twuitter account
+        const string consumerKeyString = "BEH8IXcJmeo8fPtbgWjzPJJVs";
+        const string consumerSecretString = "MP0IOA79lx8933XfJT4B2Fn08adm5Q0udmnTY38iaf0GTMdAf5";
+        const string tokenString = "223717690-qVkQXWvxl7DvTfVSvqQHjcsJW54usgzGn7beOX9z";
+        const string tokenSecret = "Qrd7Q4vgOd5XzDVZ4rFKHMW5mp1n4znwpJfST9sQB8FNs";
+
+
+        public twitterNewsfeeder()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //initiate a new twitter service
+            sTwitterService = new TwitterService(consumerKeyString, consumerSecretString);
+            //authenticate connection
+            sTwitterService.AuthenticateWith(tokenString, tokenSecret);
+
+            //when the form is loaded, initialise it with blank fields.
+            numOfTweetsTextBox.Text = "";
+            handleTextBox.Text = "";
+            resultRichTextBox.Text = "";
+        }
+
+        //search by user
+        private void userBtn_Click(object sender, EventArgs e)
+        {
+            try //try and catch Parse conversion error.
+            {
+                //change the string entered by the user to integer type and store that into the variable.
+                int numOfTweets = int.Parse(numOfTweetsTextBox.Text);
+
+                if (numOfTweets <= 0) //validate if the # Tweets is not negative value or zero.
+                {
+                    MessageBox.Show("The # Tweets cannot be negative value or zero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (handleTextBox.Text.Length == 0) //validate if anything was entered in handle/hastag field.
+                {
+                    MessageBox.Show("Please enter the @username in the Handle/HashTag field.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else //when everything is validated, run the following code.
+                {
+                    ListTweetsOnUserTimelineOptions lt = new ListTweetsOnUserTimelineOptions();
+                    lt.ScreenName = handleTextBox.Text; //get the screenname to find its tweets.
+                    lt.Count = numOfTweets; //get the number of tweets to display.
+
+                    //retrieve and store tweets from the screenname's timeline into a variable.
+                    var tweets = sTwitterService.ListTweetsOnUserTimeline(lt);
+
+                    //in case there was a result from previous search, reset the textbox.
+                    resultRichTextBox.Text = "";
+
+                    try
+                    {
+                        if (tweets.Any()) //if there is any tweet, display them as much as it was specified by the user.
+                        {
+                            foreach (var tweet in tweets)
+                            {
+                                resultRichTextBox.Text += "User Name: " + tweet.User.ScreenName + "\nTweet: " + tweet.Text + "\nTwitted Date: " + tweet.CreatedDate + "\n---------------------------------\n";
+                            }
+                        }
+                        else //if there's none, throw exception.
+                        {
+                            throw new Exception();
+                        }
+                    }
+                    catch (Exception) //handles null exception by displaying message.
+                    {
+                        resultRichTextBox.Text = "No Tweets Found.";
+                    }
+                }
+            }
+            catch (Exception) //handles exception for Parse convertion.
+            {
+                MessageBox.Show("You did not enter valid value in the \"# Tweets\" field.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }//end of userBtn_Click
+
+        //search by hashtag
+        private void hashTagBtn_Click(object sender, EventArgs e)
+        {
+            try //try and catch Parse conversion error.
+            {
+                int numOfTweets = int.Parse(numOfTweetsTextBox.Text);
+
+                if (numOfTweets <= 0) //validation for # Tweets
+                {
+                    MessageBox.Show("The number of tweets cannot be negative value or zero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    
+                }
+                else if (handleTextBox.Text.Length == 0) //validation for handle/hashtag field
+                {
+                    MessageBox.Show("Please enter a #word in the Handle/HashTag field.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else //when validation was successful, continue.
+                {
+                    SearchOptions so = new SearchOptions();
+                    so.Q = handleTextBox.Text; // the Hashtag to look for 
+                    so.Count = numOfTweets; // the number of tweets to obtain 
+
+                    var results = sTwitterService.Search(so); //store the searched results in the variable results.
+
+                    //in case there was a result from previous search, reset the textbox.
+                    resultRichTextBox.Text = "";
+
+                    try
+                    {
+                        if (results.Statuses.Any()) // verify if any tweets exist, if there's any, display them.
+                        { 
+                            foreach (var result in results.Statuses)
+                            {
+                                resultRichTextBox.Text += "User Name: " + result.User.ScreenName + "\nTweet: " + result.Text + "\nTwitted Date: " + result.CreatedDate + "\n---------------------------------\n";
+                            }
+                        }
+                        else //if there's none, throw exception.
+                        {
+                            throw new Exception();
+                        }
+                    }
+                    catch (Exception) //handles null exception by displaying message.
+                    {
+                        resultRichTextBox.Text = "No Tweets Found.";
+                    }
+                }
+            }
+            catch (Exception) //handles exception for Parse convertion.
+            {
+                MessageBox.Show("You did not enter valid value in the \"# Tweets\" field.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }//end of hashTagBtn_Click
+
+        //close the app when exit button is clicked.
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //display a message box when user clicks about menu.
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Name: Twitter Newsfeeder\n" +
+                "Author: Hae Yeon (Lucy) Kang\n" +
+                "Last Updated: 2016-03-06\n" +
+                "This application displays tweets by specified user or by hashtag as much as the number of the ."
+                , "About Twitter Newfeeder", MessageBoxButtons.OK, MessageBoxIcon.Information); 
+        }
+    }
+}
